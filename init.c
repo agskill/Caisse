6ae25d626f64b91bb2dc67679a4830755b89d9a6
b95093cf <b95093cf@hotmail.fr>
2015-01-26 21:26:21 +0100

    [ADD] Gestion des erreurs de police

--- init.c
+++ init.c
@@ -29,16 +29,23 @@ int init_SDL(SDL_Surface** screen, SDL_Rect* resolution, TTF_Font **police)
         *screen = SDL_SetVideoMode(resolution->x, resolution->y, 32,
                                                SDL_SWSURFACE|SDL_DOUBLEBUF);
 
-        police[0] = TTF_OpenFont("Microsoft Sans Serif", 20);
-        police[1] = TTF_OpenFont("Microsoft Sans Serif", 8);
-
-        if (*screen == 0)
+        police[0] = TTF_OpenFont("micross.ttf", 20);
+        police[1] = TTF_OpenFont("micross.ttf", 8);
+        if (police[0] == NULL)
+        {
+            printf("Impossible d'ouvrir la police micross.ttf\n");
+            SDL_Quit();
+            TTF_Quit();
+            return 0;
+        }
+        else if (*screen == 0)
         {
             printf("Unable to set %dx%d video: %s\n", resolution->x, resolution->y, SDL_GetError());
             SDL_Quit();
             TTF_Quit();
             return 0;
         }
-        return 1;
+        else
+            return 1;
     }
 }
