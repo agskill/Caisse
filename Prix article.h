47550e73f039679b07d0643148757f7f3e440665
npx38300 <npx38300@gmail.com>
2015-01-26 02:50:58 +0100

    constante article + modif prix

---
+++ Prix article.h
@@ -0,0 +1,63 @@
+// Article et prix 
+// entrées
+
+#define POTAGE 5.60
+#define SOUPE 6.80
+#define NEMS 4.90
+#define ROULEAUX 5
+#define ASSIETTE 6.30
+#define SALADE 4.80
+#define SN 60
+#define BB 6.70
+#define HACAU 5.10
+#define CROUSTILLANT 6.30
+
+// plats
+
+#define PLAQUES 8.20
+#define PLATS 10
+#define PORC 6.50
+#define DOUX 6.80
+#define CREVDOUX 7.30
+#define CREV 7.20
+#define CREVIMP 7.30
+#define CRABE 6.90
+#define CANARD 7.70
+#define POULET 6.50
+#define BOEUF 7.20
+#define BROCHETTE 7
+#define GAMBAS 13.50
+#define SEICHE 7.30
+#define LOTTE 7.90
+#define FM 8.20
+#define LEGUMES 3.80
+#define RIZ 30
+#define RIZCRABE 4.10
+#define MENU1 17.50
+#define MENU2 12.90
+
+
+//desserts 
+
+#define DESSERT1 3.30
+#define DESSERT2 4.70
+#define DESSERT3 5.20
+#define DESSERT4 1.90
+
+// Boissons 
+
+#define APERITIF 0
+#define DIGESTIF 0
+#define SAKE 0
+#define CAFE 0
+#define SODA 0
+#define VVIN 0
+#define QUARTVIN 0
+#define DEMINVIN 0
+#define CHAPOUTIER 0
+#define DEMICHAPOUTIER 0
+#define ESTEREL 0
+#define DEMIESTEREL 0
+#define BANDOL 0
+#define DEMIBANDOL 0
+#define ORELIE 0
